a2ab0af3e335ea3403b9b002bb383aa04ae1de75
silladus <silladus@163.com>
2017-06-07 00:01:26 +0800

    精简demo
    不必要的都删掉。
    
    备注：这只是一个演示demo，有问题是一定的，而且不会少，投入商用需要修改。

--- kchart/kchartlib/src/main/java/com/github/tifezh/kchartlib/chart/EntityImpl/KLineImpl.java
+++ kchart/kchartlib/src/main/java/com/github/tifezh/kchartlib/chart/EntityImpl/KLineImpl.java
@@ -2,7 +2,7 @@ package com.github.tifezh.kchartlib.chart.EntityImpl;
 
 /**
  * k线实体
- * Created by lianming on 2017/3/7.
+ * Created by silladus on 2017/3/7.
  */
 
 public interface KLineImpl extends CandleImpl, BOLLImpl, MACDImpl, KDJImpl, RSIImpl, VOLImpl, MINImpl {
